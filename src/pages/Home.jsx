e5b3724f78058664fc36f45bb559861265b0dec2
Yordanos A. <107444432+Yordiemma@users.noreply.github.com>
2026-03-30 09:53:06 +0200

    separate DestinationCard component from Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,29 @@
-
 import "../styles/home.css";
 import DestinationCard from "../components/DestinationCard";
 
-
 function Home() {
-    return (
-        <main>
-            <section>
-                <h1>Trip Finder</h1>
-                <p>Discover your next adeventure</p>
-            </section>
+  return (
+    <main>
+      <section>
+        <h1>Trip Finder</h1>
+        <p>Discover your next adventure</p>
+      </section>
 
-            <section>
-                <input type="text" placeholder="Search destinations.." />
-            </section>
-            <section>
-                <h2>Featured Destinations</h2>
-<DestinationCard
+      <section>
+        <input type="text" placeholder="Search destinations..." />
+      </section>
+
+      <section>
+        <h2>Featured Destinations</h2>
+
+        <DestinationCard
           image="https://images.unsplash.com/photo-1507525428034-b723cf961d3e"
           title="Bali Escape"
           location="Bali, Indonesia"
           price="$450"
           rating="4.8 ⭐"
         />
+
         <DestinationCard
           image="https://images.unsplash.com/photo-1499856871958-5b9627545d1a"
           title="Paris Getaway"
@@ -30,13 +31,14 @@ function Home() {
           price="$620"
           rating="4.7 ⭐"
         />
+      </section>
 
-            </section>
-            <section>
-                <h2>Popular Trips</h2>
-                <p>More destinations coming soon</p>
-            </section>
-        </main>
-    )
+      <section>
+        <h2>Popular Trips</h2>
+        <p>More destinations coming soon</p>
+      </section>
+    </main>
+  );
 }
+
 export default Home;
