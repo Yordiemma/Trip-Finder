24891b5f22362b92da1d245638c107ebdaed6f89
Yordanos A. <107444432+Yordiemma@users.noreply.github.com>
2026-04-01 13:09:56 +0200

    add router for home explore and manage pages

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,23 +1,91 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
-
-import Home from "../pages/Home";
 import Explore from "../pages/Explore";
-import Favorites from "../pages/Favorites";
-import Profile from "../pages/Profile";
+import Home from "../pages/Home";
+import Manage from "../pages/Manage";
 
-function AppRouter() {
+function AppRouter({
+  activities,
+  filteredActivities,
+  weather,
+  searchQuery,
+  selectedCategory,
+  selectedDay,
+  sortBy,
+  editingActivity,
+  onSearchChange,
+  onCategoryChange,
+  onDayChange,
+  onSortChange,
+  onClearFilters,
+  onAddActivity,
+  onImportActivity,
+  onUpdateActivity,
+  onDeleteActivity,
+  onEditActivity,
+  onCancelEdit,
+  onToggleFavorite,
+  feedbackMessage,
+  onDismissFeedback,
+}) {
+  const favoriteCount = activities.filter((activity) => activity.favorite).length;
+
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="explore" element={<Explore />} />
-          <Route path="favorites" element={<Favorites />} />
-          <Route path="profile" element={<Profile />} />
+        <Route
+          path="/"
+          element={
+            <MainLayout
+              totalActivities={activities.length}
+              favoriteCount={favoriteCount}
+              weather={weather}
+            />
+          }
+        >
+          <Route
+            index
+            element={<Home activities={activities} favoriteCount={favoriteCount} />}
+          />
+          <Route
+            path="explore"
+            element={
+              <Explore
+                activities={activities}
+                onImportActivity={onImportActivity}
+              />
+            }
+          />
+          <Route
+            path="manage"
+            element={
+              <Manage
+                activities={filteredActivities}
+                totalActivities={activities.length}
+                searchQuery={searchQuery}
+                selectedCategory={selectedCategory}
+                selectedDay={selectedDay}
+                sortBy={sortBy}
+                editingActivity={editingActivity}
+                onSearchChange={onSearchChange}
+                onCategoryChange={onCategoryChange}
+                onDayChange={onDayChange}
+                onSortChange={onSortChange}
+                onClearFilters={onClearFilters}
+                onAddActivity={onAddActivity}
+                onUpdateActivity={onUpdateActivity}
+                onDeleteActivity={onDeleteActivity}
+                onEditActivity={onEditActivity}
+                onCancelEdit={onCancelEdit}
+                onToggleFavorite={onToggleFavorite}
+                feedbackMessage={feedbackMessage}
+                onDismissFeedback={onDismissFeedback}
+              />
+            }
+          />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
