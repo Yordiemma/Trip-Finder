9655766367886b2656f750f364ec1e31b5dcfd0d
Yordanos A. <107444432+Yordiemma@users.noreply.github.com>
2026-04-01 13:13:55 +0200

    add filter bar component

---
+++ src/components/FilterBar.jsx
@@ -0,0 +1,62 @@
+import { categoryOptions, dayOptions } from "../utils/activityOptions";
+
+function FilterBar({
+  selectedCategory,
+  selectedDay,
+  sortBy,
+  onCategoryChange,
+  onDayChange,
+  onSortChange,
+  onClearFilters,
+}) {
+  return (
+    <div className="filter-bar">
+      <label className="filter-group">
+        <span>Category</span>
+        <select
+          value={selectedCategory}
+          onChange={(event) => onCategoryChange(event.target.value)}
+        >
+          <option value="All">All categories</option>
+          {categoryOptions.map((category) => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+      </label>
+
+      <label className="filter-group">
+        <span>Day</span>
+        <select
+          value={selectedDay}
+          onChange={(event) => onDayChange(event.target.value)}
+        >
+          <option value="All">All days</option>
+          {dayOptions.map((day) => (
+            <option key={day} value={day}>
+              {day}
+            </option>
+          ))}
+        </select>
+      </label>
+
+      <label className="filter-group">
+        <span>Sort by</span>
+        <select value={sortBy} onChange={(event) => onSortChange(event.target.value)}>
+          <option value="featured">Featured order</option>
+          <option value="title">Title</option>
+          <option value="day">Day</option>
+          <option value="price">Price level</option>
+          <option value="favorites">Favorites first</option>
+        </select>
+      </label>
+
+      <button type="button" className="button button--secondary" onClick={onClearFilters}>
+        Clear filters
+      </button>
+    </div>
+  );
+}
+
+export default FilterBar;
